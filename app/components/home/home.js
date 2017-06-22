0e503a1df4bc31e8057ec268266f89d7d296f989
Ventsislav Georgiev <ventsislav.georgiev@telerik.com>
2017-06-22 14:01:43 +0300

    Add graph to home page

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -1,23 +1,26 @@
 define((require) => {
+    const _ = require("lodash");
     const template = require("text!./home.html");
     const dataService = require("app/services/data-service");
+    const cytoscape = require("cytoscape");
 
     return {
         template,
         data() {
             return {
                 searchValue: "",
-                customer: null
+                customer: null,
+                cy: null
             };
         },
 
         methods: {
             search(email) {
                 if(email) {
-                    dataService.getJourneyByEmail(email)
-                        .then((data) => { 
-                            this.customer = data; 
-                            console.log(data); 
+                    dataService.getJourney(email)
+                        .then((data) => {
+                            this.customer = data;
+                            console.log(data);
                         });
                 }
             },
@@ -26,7 +29,74 @@ define((require) => {
                 console.log("clear");
                 this.searchValue = "";
                 this.customer = null;
+            },
+
+            _createGraph(el, data) {
+                this._cy = cytoscape({
+                    container: el,
+
+                    boxSelectionEnabled: false,
+                    autounselectify: true,
+
+                    style: cytoscape.stylesheet()
+                        .selector('node')
+                        .css({
+                            'content': 'data(name)',
+                            'text-valign': 'center',
+                            'color': 'white',
+                            'text-outline-width': 2,
+                            'background-color': '#999',
+                            'text-outline-color': '#999'
+                        })
+                        .selector('edge')
+                        .css({
+                            'curve-style': 'bezier',
+                            'target-arrow-shape': 'triangle',
+                            'target-arrow-color': '#ccc',
+                            'line-color': '#ccc',
+                            'width': 1
+                        })
+                        .selector(':selected')
+                        .css({
+                            'background-color': 'black',
+                            'line-color': 'black',
+                            'target-arrow-color': 'black',
+                            'source-arrow-color': 'black'
+                        })
+                        .selector('.faded')
+                        .css({
+                            'opacity': 0.25,
+                            'text-opacity': 0
+                        }),
+
+                    elements: {
+                        nodes: _.map(data.nodes, (node) => ({
+                            data: {
+                                id: node.id,
+                                name: node.label
+                            }
+                        })),
+                        edges: _.map(data.links, (link) => ({
+                            data: {
+                                source: data.nodes[link.source].id,
+                                target: data.nodes[link.target].id
+                            }
+                        }))
+                    },
+
+                    layout: {
+                        name: 'grid',
+                        padding: 10
+                    }
+                });
             }
+        },
+
+        mounted() {
+            dataService.getJourney()
+                .then((data) => {
+                    this._createGraph(this.$refs.graph, data);
+                });
         }
     };
 });
